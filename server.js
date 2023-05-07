e5b9510c7cd7bb59c5c3e84deb505ee15e973a66
PhilippWinston <philippwinston@gmail.com>
2023-05-07 17:55:59 -0500

    relocated the routes for API and HTML, added in route handling, added note.js for read and write Functions, added a couple of tests to the db.json

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes/index");
+const htmlRoutes = require("./routes/htmlRoutes/index");
 const PORT = process.env.port || 3001;
-const path = require("path");
 
 const app = express();
 
@@ -12,18 +11,6 @@ app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
 app.use(express.static("public"));
-
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
-
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
-
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 👾`)
