8e87e235fa1f9b0522e71f5057618ddbb0e23532
PhilippWinston <philippwinston@gmail.com>
2023-05-08 21:51:50 -0500

    updating

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-app.use(express.static("public"));
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 👾`)
